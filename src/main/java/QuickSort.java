edae194fd4c32f7d153079e2ed4493d774a1c4d8
Magdalena Gola <turcza.magdalena@gmail.com>
2020-08-10 15:28:33 +0200

    Add quick sort algorithm for a list param

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+class QuickSort {
+
+    /**
+     * Sorts the given List in place
+     *
+     * @param numbers the List of Integers to sort. Throws an error if its null
+     */
+    static void quickSort(List<Integer> numbers) {
+        if (numbers == null) throw new IllegalArgumentException();
+        if (numbers.size() <= 1) return;
+        int scrollingIndex = 0;
+        int positionIndex = 0;
+        int pivotIndex = numbers.size() / 2;
+        Integer pivot = numbers.get(pivotIndex);
+
+        //replacing last element with the pivot
+        numbers.set(pivotIndex, numbers.get(numbers.size() - 1));
+        numbers.set(numbers.size() - 1, pivot);
+
+        //dividing collection into two parts, where the elements in the left partition are less or equal then all elements in the right partition;
+        //partitions are divided by pivot
+        Integer comparingElement;
+        for (; scrollingIndex < numbers.size() - 2; scrollingIndex++) {
+            comparingElement = numbers.get(scrollingIndex);
+            if (comparingElement > pivot) continue;
+            numbers.set(scrollingIndex, numbers.get(positionIndex));
+            numbers.set(positionIndex, comparingElement);
+            positionIndex++;
+        }
+        numbers.set(numbers.size() - 1, numbers.get(positionIndex));
+        numbers.set(positionIndex, pivot);
+
+        //sort recursively each partition
+        List<Integer> leftPartition = numbers.subList(0, positionIndex);
+        List<Integer> rightPartition = numbers.subList(positionIndex + 1, numbers.size());
+        if (positionIndex > 0) {
+            quickSort(leftPartition);
+        }
+        if (positionIndex < numbers.size()) {
+            quickSort(rightPartition);
+        }
+    }
+}
