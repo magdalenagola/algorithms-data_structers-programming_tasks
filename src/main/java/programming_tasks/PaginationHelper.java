e9056155d9d78e1c977660e2e061f5c95057c94f
Magdalena Gola <turcza.magdalena@gmail.com>
2020-08-11 12:51:31 +0200

    Add solution to PaginationHelper kata

---
+++ src/main/java/programming_tasks/PaginationHelper.java
@@ -0,0 +1,71 @@
+package programming_tasks;
+
+import java.util.List;
+
+/**
+5 kyu
+
+For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+
+The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+The following are some examples of how this class is used:
+
+PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+helper.pageCount(); //should == 2
+helper.itemCount(); //should == 6
+helper.pageItemCount(0); //should == 4
+helper.pageItemCount(1); // last page - should == 2
+helper.pageItemCount(2); // should == -1 since the page is invalid
+helper.pageIndex(5); //should == 1 (zero based index)
+helper.pageIndex(2); //should == 0
+helper.pageIndex(20); //should == -1
+helper.pageIndex(-10); //should == -1
+ */
+public class PaginationHelper<I> {
+    List<I> collection;
+    int itemsPerPage;
+
+    /**
+     * The constructor takes in an array of items and a integer indicating how many
+     * items fit within a single page
+     */
+    public PaginationHelper(List<I> collection, int itemsPerPage) {
+        this.collection = collection;
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    /**
+     * returns the number of items within the entire collection
+     */
+    public int itemCount() {
+        return collection.size();
+    }
+
+    /**
+     * returns the number of pages
+     */
+    public int pageCount() {
+        int numberOfPages = itemCount();
+        return numberOfPages % itemsPerPage == 0 ? numberOfPages/itemsPerPage : numberOfPages/itemsPerPage + 1;
+    }
+
+    /**
+     * returns the number of items on the current page. page_index is zero based.
+     * this method should return -1 for pageIndex values that are out of range
+     */
+    public int pageItemCount(int pageIndex) {
+        if (pageIndex >= pageCount()) return -1;
+        if (pageIndex < pageCount() -1) return  itemsPerPage;
+        return itemCount() % itemsPerPage == 0 ? itemsPerPage : itemCount() % itemsPerPage;
+    }
+
+    /**
+     * determines what page an item is on. Zero based indexes
+     * this method should return -1 for itemIndex values that are out of range
+     */
+    public int pageIndex(int itemIndex) {
+        if (itemIndex >= itemCount() || itemIndex < 0) return -1;
+        return itemIndex/itemsPerPage;
+    }
+}
